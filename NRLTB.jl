171725e0d66b33cfb1f5c91d861eb0c1b15ded8f
Christoph Ortner <c.ortner@warwick.ac.uk>
2015-10-15 11:36:36 +0100

    added NRLTB.jl

---
+++ NRLTB.jl
@@ -0,0 +1,30 @@
+
+
+module NRLTB
+
+using Potentials
+
+
+
+type NRLParams
+    
+end
+
+
+type NRLhop <: PairPotential
+
+end
+
+
+type NRLos <: SitePotential
+
+end
+
+
+type NRLoverlap <: PairPotential
+
+end
+
+
+
+end
