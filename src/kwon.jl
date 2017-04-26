eb5ead923f586184bd78f592cbd60153813b9831
Christoph Ortner <c.ortner@warwick.ac.uk>
2017-04-26 22:06:09 +0100

    initial implementation of KwonTB

--- src/kwon.jl
+++ src/kwon.jl
@@ -1,15 +1,33 @@
 
+module Kwon
 
+using Parameters
+using TightBinding: SKHamiltonian, hop, TBModel
+using JuLIP: cutoff
+using JuLIP.Potentials: fcut, fcut_d,
+                     NullPotential, SplineCutoff, AnalyticPotential
 
-# α   1     2    3     4
-#    ssσ   spσ  ppσ   ppπ
+"""
+`KwonHamiltonian <: SKHamiltonian{ORTHOGONAL, 4}`
 
-@with_kw type KwonHamiltonian
+Hamiltonian for an orthogonal sp TB model of Si developed by Kwon et al [1].
+
+This implementation deviates  from [1] in how the cut-off is applied:
+instead of "patching" a cubic spline between r1 and rcut, we simply multiply
+with a quintic spline on the interval [0.5 (rcut + r0), rcut].
+
+[1] I. Kwon, R. Biswas, C. Z. Wang, K. M. Ho and C. M. Soukoulis.
+Transferable tight-binding models for silicon.
+Phys Rev B 49 (11), 1994.
+"""
+@with_kw immutable KwonHamiltonian <: SKHamiltonian{ORTHOGONAL, 4}
    r0::Float64 = 2.360352   # Å
    Es::Float64 = 5.25       # eV
    Ep::Float64 = 1.2        # eV
    E0::Float64 = 8.7393204  # eV
    # ------------------------------- Electronic Parameters
+   # α   1     2    3     4
+   #    ssσ   spσ  ppσ   ppπ
    hr0::NTuple{4, Float64} = (-2.038, 1.745, 2.75, -1.075)
    nc::NTuple{4, Float64} = (9.5, 8.5, 7.5, 7.5)
    rc::NTuple{4, Float64} = (3.4, 3.55, 3.7, 3.7)
@@ -18,73 +36,36 @@
    mc::Float64 = 13.017
    dc::Float64 = 3.66995     # Å
    C::NTuple{4, Float64} = (2.1604385, -0.1384393, 5.8398423e-3, -8.0263577e-5)
-   # ----------------------------- storage
-   mat::MMatrix{4, 4, Float64, 16} = MMatrix{4, 4}(zeros(16))
+   # --------------------------------
+   r1::Float64 = 3.260176     # Å  (start of cut-off) >>> different r1 from Kwon paper
+   rcut::Float64 = 4.16       # Å  (end of cut-off)
 end
 
-# hopping function
-h(tbm::KwonParams, r, α) = ( tbm.hr0[α] * (tbm.r0 / r)^2 *
-   exp( - 2 * (r/tbm.rc[α])^tbm.nc[α] + 2 * (tbm.r0/tbm.rc[α])^tbm.nc[α] ) )
-dh_rdiff = rdiff(h, (KwonParams, Float64, Int); order = 1, allorders = false, ignore = (:tbm, :α))
-dh(tbm::KwonParams, r, α) = dh_rdiff(tbm, r, α)[1]
+kwon_hop(H::KwonHamiltonian, r, α) = ( H.hr0[α] * (H.r0 / r)^2 *
+               exp( - 2 * (r/H.rc[α])^H.nc[α] + 2 * (H.r0/H.rc[α])^H.nc[α] ) )
 
-# embedding function
-f(tbm::KwonParams, x) =
-   x * (tbm.C[1] + x * (tbm.C[2] + x * (tbm.C[3] + x * tbm.C[4])))
-df_rdiff = rdiff(f, (KwonParams, Float64); order = 1, allorders = false, ignore = (:tbm,))
-df(tbm::KwonParams, x) = df_rdiff(tbm, x)[1]
+hop(H::KwonHamiltonian, r, α) = kwon_hop(H, r, α) *
+                                 JuLIP.Potentials.fcut(r, H.r1, H.rcut)
 
-# pair potential
-phi(tbm::KwonParams, r) = ( (tbm.r0/r)^tbm.m *
-   exp( - tbm.m * (r/tbm.dc)^tbm.mc + tbm.m * (tbm.r0/tbm.dc)^tbm.mc ) )
+cutoff(H::KwonHamiltonian) = H.rcut
+
+# embedding function C1 x + C2 x^2 + C3 x^3 + C3 x^4
+KwonEmbedding(H::KwonHamiltonian) =
+   AnalyticPotential( :( r * ($(H.C[1]) + r * ($(H.C[2]) + r * ($(H.C[3]) + r * $(H.C[4])))) ) )
+
+# electron density: (r0/r)^m * exp( - m * (r/dc)^mc + m * (r0/dc)^mc )
+KwonElDensity(H::KwonHamiltonian) = (
+   AnalyticPotential( :( ($(H.r0)/r)^($(H.m)) *
+         exp( - $(H.m) * (r/$(H.dc))^($(H.mc)) + $(H.m) * ($(H.r0/H.dc))^($(H.mc)) ) ) )
+   * SplineCutoff(H.r1, H.rcut)
+)
+
+KwonEAM(H::KwonHamiltonian) = EAM(ZeroPairPotential(), KwonElDensity(H), KwonEmbedding(H))
 
 
-
-
-
-@pot type KwonHop <: PairPotential
-    params::KwonParams
+function KwonTBModel(; potential = NullSmearing(), bzquad = NullBZQ())
+   H = KwonHamiltonian()
+   return TBModel(H, KwonEAM(H), potential, bzquad, 1e-6)
 end
-
-function evaluate!(p::KwonHop, r, R, H, temp)
-   U = R / r
-   for n = 1:4
-      temp[n] = h(p.params, r, n)
-   end
-   sk4!(U, temp, H)
-   return H
-end
-
-function grad!(p::KwonHop, r, R, dH)
-   temp = [dh(p.params, r, n) for n = 1:4]
-   d_mat_local!(r/BOHR, R/BOHR, p.elem, :dH, dH)
-   scale!(dH, 1.0/BOHR)
-end
-
-
-
-# Kwon TB on-site term
-
-@pot type KwonOnsite <: Potential
-   elem::KwonParams
-end
-
-
-# Kwon TB Overlap matrix: this is orthogonal TB i.e. just an identity
-
-@pot type KwonOverlap <: PairPotential
-    elem::KwonParams
-end
-
-function evaluate!(p::KwonOverlap, r, R, M, temp)
-   fill!(M, 0.0)
-   for n = 1:3; M[n,n] = 1.0; end
-   return M
-end
-
-grad!(p::KwonOverlap, r, R, dM::Array{Float64, 3}) = fill!(dM, 0.0)
-
-
-
 
 end
